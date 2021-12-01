72c6f942f1ad820d2fbb8fa923801eb61a9baad1
bsurwash <siddhantsurwashi@gmail.com>
2021-12-01 16:01:49 +0530

    Updated MicroService

---
+++ Backend/BookingTickets-Service/TicketOrder-Service/src/main/java/com/ticket/TicketOrderService/Service/BookingService.java
@@ -0,0 +1,7 @@
+package com.ticket.TicketOrderService.Service;
+
+import com.ticket.TicketOrderService.Entity.BookingTicket;
+
+public interface BookingService {
+    public BookingTicket getBooking(String trainId);
+}
